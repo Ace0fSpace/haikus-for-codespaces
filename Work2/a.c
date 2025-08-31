17508873042d7d2db0eb6433a0c5c396fa658b7b
Ace0fSpace <milkmaster173@gmail.com>
2025-08-31 05:09:47 +0000

    HW-1

--- Work2/a.c
+++ Work2/a.c
@@ -3,15 +3,29 @@
 
 int main()
 {
-    char FirstLetter = 'W';
-    int age = 19;
-    float height = 5.9;
-    bool isStudent = true;
 
-    printf("First letter of last name: %c\n", FirstLetter);
-    printf("Age: %d\n", age);
-    printf("Height: %.2f feet\n", height);
-    printf("Student: %s\n", isStudent ? "true" : "false");
+    int var2; 
+    char var1; 
+    double var3; 
+    bool var4;
 
+    printf("Enter the first letter of your last name: ");
+    scanf("  %c", &var1);
+
+    printf("Enter your age: ");
+    scanf("%d", &var2);
+
+    printf("Enter your height in feet: ");
+    scanf("%lf", &var3);
+
+    printf("Are you a student: ");
+    scanf("%d", (int*)&var4);
+
+    printf("First letter of last name: %c\n", var1);
+    printf("Age: %d\n", var2);
+    printf("Height: %.2f feet\n", var3);
+    printf("Student: %s\n", var4 ? "Yes" : "No");
+    
     return 0;
-}
+} 
+   
