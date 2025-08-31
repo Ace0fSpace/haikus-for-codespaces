a579d1aa89f98fbb6eb490d2c4d97c9f2e0f2dbb
Ace0fSpace <milkmaster173@gmail.com>
2025-08-31 05:25:00 +0000

    In class assignment 2

--- Work2/Casting.c
+++ Work2/Casting.c
@@ -5,11 +5,11 @@ int main()
 
     int var1 ,var2;
 
-    printf("Enter the first number");
+    printf("Enter the first number: ");
     scanf("%d",&var1);
 
     
-    printf("Enter the second number");
+    printf("Enter the second number: ");
     scanf("%d",&var2);
 
     printf("The div of 2 numbers = %.2f" ,(float)var1/var2);
@@ -20,7 +20,30 @@ int main()
     
     printf("The mult of 2 numbers = %d" ,(int)var1*var2);
 
-    printf("The rem of 2 numbers = %d" ,(int)var1%var2);
+    printf("The rem of 2 numbers = %d\n" ,(int)var1%var2);
+    
+    
+    int min;
+
+    printf("Enter the number of minutes:");
+    scanf("%d",&min);
+
+    printf("The number of hours = %d The number of mins = %d\n",min/60,min%60);
+    
+    int var3 ,var4;
+
+    printf("Enter the first number: ");
+    scanf("%d",&var3);
+
+    
+    printf("Enter the second number: ");
+    scanf("%d",&var4);
+    printf("less than=%d\n", var3<var4);
+    printf("less than or equal to=%d\n", var3<=var4);
+    printf("greater than=%d\n", var3>var4);
+    printf("greater than or equal to=%d\n", var3>=var4);
+    printf("equal to=%d\n", var3=var4);
+    printf("not equal to=%d\n", var3!=var4);
     
     return 0;
 }
