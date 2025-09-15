8cbe03699fd26b64c6efadf684a45b79362ba9c7
Ace0fSpace <milkmaster173@gmail.com>
2025-09-15 16:20:27 +0000

    man idk if this is right

--- Work4/4loops.c
+++ Work4/4loops.c
@@ -1,33 +1,52 @@
-// Write a C program that implements a simple menu system with the following options:
-// Calculate the power of a number (ask the user for a base and an exponent). ( NOTE : Do not use Math.h functions) 
-// Find the sum of numbers from 1 up to a given number n. (ask the user for n)
-// Exit the program.
-// The program should repeatedly display the menu until the user chooses to exit. Use appropriate conditional logic to handle the choices.
-// Challenge Activity (Optional):
-// Extend your program by adding an option to check whether the entered number is a prime number.
 #include<stdio.h>
 
 int main()
 {
     int num,exp, power=1;
 
-    printf("Enter the base number: ");
-    scanf("%d",&num);
-    printf("Enter the exponent: ");
-    scanf("%d",&exp);
-    
 
-    for(int i=0;i<=exp;i++)
+    do
     {
-        if(i==0)
-            power= 1;
-        else{
-        power=power*num;
+        printf("Enter choice \n 1.Power 2.Sumton 3.Exit :");
+        scanf("%d", &choice);
+
+        if(choice ==1)
+        {
+            printf("Enter the base number: ");
+            scanf("%d",&num);
+
+            printf("Enter the exponent: ");
+            scanf("%d",&exp);
+        
+            for(int i=0;i<=exp;i++)
+        {
+            if(i==0)
+                power= 1;
+            else{
+            power=power*num;
+            }
+            
+        }
+        else if(choice ==2)
+        {
+            printf("%d",power);
+
+
+            printf("Enter the base number: ");
+            scanf("%d",&num);
+         int sum=0;
+
+            while(num>0)
+        {
+            sum= sum+ num;
+            num--;
+        }
+
+            printf("Sum =%d",sum);
+    }while(choice !=3);
         }
         
-    }
 
-    printf("%d",power);
-
+        
     
 }
--- Work4/forloopbreak.c
+++ Work4/forloopbreak.c
@@ -5,7 +5,6 @@ int main()
 
     for( int number=0;number<10;number++)
     {
-            
             if(number%2==1)
                 break;
                 printf("Number =%d ", number);
