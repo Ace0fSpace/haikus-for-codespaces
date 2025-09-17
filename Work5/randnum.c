ab4efa57a2d261dab6dfc5d43370c0d9c86eb129
Ace0fSpace <milkmaster173@gmail.com>
2025-09-17 16:40:19 +0000

    Work5

---
+++ Work5/randnum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(void)
+
+{
+    int lower, upper
+    srand(time(NULL));
+    int randomValue = rand();
+    printf("Generated random number: %d\n", randomValue);
+
+    printf("Enter lower limit");
+    scanf("%d", & lower);
+
+    printf("Enter upper limit");
+    scanf("%d", upper);
+    int lower = 1;
+    int upper = 10;
+    int randomInRange = (randomValue % (upper - lower + 2)) + lower;
+    printf("Random number between %d and %d: %d\n", lower, upper, randomInRange);
+}
