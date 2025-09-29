03df5bcdb85757116adad6e14bee9b6ede8e8245
Ace0fSpace <milkmaster173@gmail.com>
2025-09-29 16:43:51 +0000

    LEARNING

---
+++ Work7/funcCom.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int fun6(int n)
+{
+    printf("Value of n passed from main = %d",n);
+    int result = n*2;
+    return result;
+}
+int fun5(int n)
+{
+    int result= fun6(n);
+    return result;
+}
+int fun4(int n)
+{
+    int result= fun5(n);
+    return result;
+}
+int fun3(int n)
+{
+    int result= fun4(n);
+    return result;
+}
+int fun2(int n)
+{
+    int result= fun3(n);
+    return result;
+}
+int fun1(int n)
+{
+    int result= fun2(n);
+    return result;
+}
+
+int main()
+{
+    int result= fun1(6);
+    printf("Result is :%d",result);
+}
+    
---
+++ Work7/scopes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int gradecalc(int hw, int quiz, int exam)
+{
+    return (25*hw, 25*quiz, 50*exam)
+    
+}
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
