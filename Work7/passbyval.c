6a96a2ca2019ef4520a358f4ca43c87b9cc46a0f
Ace0fSpace <milkmaster173@gmail.com>
2025-10-06 16:45:51 +0000

    stuff

---
+++ Work7/passbyval.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+void func(int a)
+{
+    a=6;
+    printf("Value of a in function = %d\n",a);
+}
+
+void main()
+{
+    int b=8;
+    func(b);
+    printf("Value of b in main %d\n",b);
+}
