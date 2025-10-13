8bc9ac3a3ae1c4c7fa5d8667fb34663aa5b490a2
Ace0fSpace <milkmaster173@gmail.com>
2025-10-13 16:44:01 +0000

    lol

---
+++ Work9/Reccur.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int b=2, e=2, tot;
+
+
+int main(tot)(int b, int e);
+{
+    if(e==0)
+        return 1;
+    else
+        return(b*pow(b,e-1));
+}
+
+// int Power(int base, int exponent){long result = 1;   while (exponent!=0){result *=base;  --exponent;} return result;}
