f6f7fd7b4cb3561f8de01daa3d66d71723c3ad67
Ace0fSpace <milkmaster173@gmail.com>
2025-11-03 17:47:45 +0000

    arrays.c in class

---
+++ Work12/array.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[10],arr2[10],arr3[3][3],arr4[3][3];
+
+    for(int i=0;i<10;i++)
+    {
+        arr[i]=(i+1)*(i+1);
+        arr2[i]= i*i*i;
+    }
+
+    int even=2,odd=1;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            arr3[i][j]=even;
+            arr4[i][j]=odd;
+            even=even+2;
+            odd=odd+2;
+        }
+    }
+    printf("1D arrays are:\n");
+    printf("1st Array(squares of n): ");
+        for(int i=0;i<10;i++)
+        {
+            printf("%d  ",arr[i]);
+        }
+    printf("2nd Array(): ");
+    for(int i=0;i<10;i++)
+    {
+        printf("%d  ",arr2[i]);
+    }
+printf("2D arrays are: \n");
+printf("1st 2D array:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",arr3[i][j]);
+        }
+        printf("\n");
+    }
+printf("2nd 2D array:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",arr4[i][j]);
+        }
+        printf("\n");
+    }
+    //add_two1d();
+    //sub_two1d();
+}
