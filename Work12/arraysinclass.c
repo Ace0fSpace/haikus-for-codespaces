02077d90a9d7ff9472af35df2197ba674adf77c2
Ace0fSpace <milkmaster173@gmail.com>
2025-11-05 17:31:36 +0000

    in class assignment

---
+++ Work12/arraysinclass.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+
+void add_two1Darray(int size1, int p[size1], int size2, int q[size2])
+{
+    int r[size1];
+    printf("third array :\n");
+    for(int i=0; i<size1; i++)
+    {
+        r[i]=p[i]+q[i];
+        printf("%d ",r[i]);
+    }
+    printf("\n");
+}
+
+void diff_2Darray(int rows, int cols, int p[rows][cols], int q[rows][cols])
+{
+    int r[rows][cols];
+    printf("fourth array :\n");
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            r[i][j]= p[i][j] - q[i][j];
+
+            printf("%d ",r[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int arr[10],arr2[10],arr3[3][3],arr4[3][3];
+
+    for(int i=0;i<10;i++)
+    {
+        arr[i]=(i+1)*(i+1);
+        arr2[i]= i*i*i;
+    }
+
+    int even=2,odd=1;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            arr3[i][j]=even;
+            arr4[i][j]=odd;
+            even=even+2;
+            odd=odd+2;
+        }
+    }
+    printf("1D arrays are:\n");
+    printf("1st Array(squares of n): ");
+        for(int i=0;i<10;i++)
+        {
+            printf("%d  ",arr[i]);
+        }
+        printf("\n");
+    printf("2nd Array(): ");
+    for(int i=0;i<10;i++)
+    {
+        printf("%d  ",arr2[i]);
+    }
+    printf("\n");
+printf("2D arrays are: \n");
+printf("1st 2D array:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",arr3[i][j]);
+        }
+        printf("\n");
+    }
+printf("2nd 2D array:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",arr4[i][j]);
+        }
+        printf("\n");
+    }
+    add_two1Darray(10,arr,10,arr2);
+    diff_2Darray(3,3,arr3,arr4);
+
+    return 0;
+}
