39ab3d907c07e69f84886f4c02ca32998cddae0d
Ace0fSpace <milkmaster173@gmail.com>
2025-11-05 17:42:17 +0000

    chararrays :)

---
+++ Work12/chararray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void (name[100])
+{
+    printf("Enter a name:");
+    scanf("%s"*name);
+    fgets(name, sizeof(name), stdin);
+    printf("Entered name = %s", name);
+    return 0:
+
+}
+
+int main()
+{
+    char a[10]={'b','a','l','l','s','\0'};
+
+    printf("%s",a);
+    
+    return 0;
+    
+}
