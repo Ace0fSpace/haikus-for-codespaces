b7120dac832c2ae6e93c698303605044acde465c
Ace0fSpace <milkmaster173@gmail.com>
2025-11-19 17:48:24 +0000

    midterm

---
+++ Work14/moremidterm2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int computeSum( (1) , (2) )       
+{
+    int sum = 0;
+
+    for(int i = 0; i < (3); i++)   
+    {
+        sum = sum + (4);          
+    }
+
+    return (5);                    
+}
+
+int main()
+{
+    int arr[5];
+
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d", (6));          
+    }
+
+    int result = computeSum( (7) , (8) );
+
+    printf("Sum = %d\n", result);
+
+    return 0;
+}
--- Work7/RPS.c
+++ Work7/RPS.c
@@ -2,40 +2,108 @@
 #include<stdlib.h>
 #include<time.h>
 
-int getCompChoice()
+// 1 - Rock, 2-Paper & 3 - scissors
+int getComputerChoice() 
 {
-    return (rand()%3+1);
+    return (rand()%3)+1;
 }
 
-int decideWinner(int playerChoice, int computerChoice)
+int decideWinner(int playerChoice, int computerChoice) 
 {
+
+    // Capture scenario for tie
     if(playerChoice==computerChoice)
         return 0;
-    if(playerChoice == 1 && computerChoice == 3), (playerChoice == 2 && computerChoice == 1), (playerChoice == 3 && computerChoice == 2);
+    // Capture all scenarios for win
+    if((playerChoice ==1 && computerChoice == 3) || (playerChoice ==2 && computerChoice == 1)||(playerChoice ==3 && computerChoice == 1))
         return 1;
+    //loss scenario
     return -1;
+}
+
+void choiceDecrypt(int ch)
+{
+    if(ch ==1)
+        printf(" Rock!");
+    else if (ch==2)
+        printf(" paper");
+    else if ( ch==3)
+        printf(" Scissor");
+    else
+        printf(" Invalid choice");
 }
 
 int main()
 {
-    int rch,userCh;
-
+    int repeat;
     do
     {
-     printf("Enter number of rounds 5 or 10:\n");
-     scanf("%d",&rch);
+            int  userCh,rch,userWin=0,CompWin=0,ties=0;
 
-    }while(rch != 5 && rch !=10)
-    
-    for(int i=0; i<rch; i++)
-    {
-        printf("1. Rock 2. Paper 3.Scissors\n")
-        printf("Enter users choice");
-        scanf("%d",&userCh)
+            do
+            {
+                printf("Enter number of rounds 5 or 10:");
+                scanf("%d",&rch);
 
-        int comCh = getComputerChoice();
+            } while (rch != 5 && rch !=10);
 
-        int result = decideWinner(userCh,comCh);
-    }
-    
+            for(int i=0;i<rch;i++)
+            {
+                printf("1. Rock 2.Paper 3. scissors \n");
+                printf("Enter users choice");
+                scanf("%d",&userCh);
+
+                int comCh = getComputerChoice();
+
+                int result = decideWinner(userCh,comCh);
+
+                printf("User plays");
+                choiceDecrypt(userCh);
+
+                printf("Computer plays");
+                choiceDecrypt(comCh);
+                printf("\n");
+
+                if( result ==0 )
+                {
+                    printf("Its a tie\n");
+                    ties++;
+                }
+                else if(result ==1)
+                {
+                    printf("User wins\n");
+                    userWin++;
+                }
+                else    
+                {
+                    printf("Computer wins\n");
+                    CompWin++;
+                }
+
+                // Break early
+                if ((CompWin>=(rch/2+1))||(userWin>=(rch/2+1)))
+                {
+                    break;
+                }
+
+            }
+            
+        printf("=== Final Results ===\n");
+
+            printf("You: %d  Computer: %d  Ties: %d",userWin,CompWin,ties);
+
+        if(userWin>CompWin)
+        {
+            printf("\nUser wins the match!");
+        }
+        else if(CompWin>userWin)
+        {
+            printf("\nComputer wins the match!");
+        }
+        else
+            printf("\nIts a tie super rare!");
+
+    printf("\nDo you want to continue? 1 for Y or 0 for N");
+    scanf("%d",&repeat);
+    }while(repeat);
 }
